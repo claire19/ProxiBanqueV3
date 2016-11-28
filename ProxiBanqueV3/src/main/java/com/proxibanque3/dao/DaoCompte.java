73ea70a17a0b0d454beda7b8d84153d1f2319439
claire19 <claire.steinm@gmail.com>
2016-11-28 10:27:36 +0100

    Troisieme commit

---
+++ ProxiBanqueV3/src/main/java/com/proxibanque3/dao/DaoCompte.java
@@ -0,0 +1,22 @@
+package com.proxibanque3.dao;
+
+import java.util.Map;
+
+import com.proxibanque3.model.Compte;
+
+
+public interface DaoCompte {
+	
+	void createCompteDao(Compte compte);
+
+	Map<Long, Compte> readAllCompteDao();
+
+	Compte readCompteDaoById(long numeroCompte);
+
+	void updateCompteDao(Compte compte);
+
+	void deleteCompteDao(Compte compte);
+	
+	long createNumeroCompte();
+
+}
---
+++ ProxiBanqueV3/src/main/java/com/proxibanque3/dao/SQLDaoCompte.java
@@ -0,0 +1,45 @@
+package com.proxibanque3.dao;
+
+import java.util.Map;
+
+import com.proxibanque3.model.Compte;
+
+public class SQLDaoCompte implements DaoCompte {
+
+	@Override
+	public void createCompteDao(Compte compte) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Map<Long, Compte> readAllCompteDao() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Compte readCompteDaoById(long numeroCompte) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void updateCompteDao(Compte compte) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteCompteDao(Compte compte) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public long createNumeroCompte() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
